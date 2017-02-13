e5df2c9a5205e300fdcf52e0b8c4546b97c188e1
Yuan Zihong <yuanzhchina@gmail.com>
2017-02-14 00:34:31 +0800

    update read mySQL

--- dowloadAndRead.R
+++ dowloadAndRead.R
@@ -45,7 +45,25 @@ myjson <- toJSON(iris, pretty = TRUE)
 cat(myjson)
 
 #mySQL
+library(RMySQL)
+ucscDb <- dbConnect(MySQL(), user="genome", 
+                    host="genome-mysql.cse.ucsc.edu")
+result <- dbGetQuery(ucscDb, "show databases;"); dbDisconnect(ucscDb);
 
-
-
-
+hg19 <- dbConnect(MySQL(), user="genome", db ="hg19",
+                  host="genome-mysql.cse.ucsc.edu")
+allTables <- dbListTables(hg19)
+length(allTables)
+allTables[1:5]
+dbListFields(hg19, "HInv")
+#nrow
+dbGetQuery(hg19, "select count(*) from HInv")
+affyData <- dbReadTable(hg19, "affyU133Plus2")
+head(affyData)
+#select a specific subset
+query <- dbSendQuery(hg19, "select * from affyU133Plus2 where misMatches between 1 and 3")
+affyMis <- fetch(query); quantile(affyMis$misMatches)
+affyMisSmall <- fetch(query, n=10); dbClearResult(query)
+dim(affyMisSmall)
+#close connection
+dbDisconnect(hg19)
